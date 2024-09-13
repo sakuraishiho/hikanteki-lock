f98cea63df5805f53752936d9d8c7c288b64e040
sakura <ichigomilk35412@gmail.com>
2024-09-13 19:35:52 +0900

    悲観的ロック実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,10 +5,11 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :ordered_lists
 
   def update_total_quantity
-    self.ordered_lists.each do |line_item|
-      item = Item.find_by(id: line_item.item_id)
-      item.total_quantity += line_item.quantity
-      item.save!
+    ActiveRecord::Base.transaction do
+      # 悲観的ロックをかけて他のプロセスが同じデータを更新しないようにする
+      item = ordered_lists.first.item.lock!
+      new_total = item.total_quantity + ordered_lists.sum(:quantity)
+      item.update!(total_quantity: new_total)
     end
   end
 end
